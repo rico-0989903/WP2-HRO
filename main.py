383a6f4a6fe9484c171db1bba36cbcf677f94bcd
Jelle-1035608 <Benardjelle@gmail.com>
2023-02-28 13:00:01 +0100

    ADD: Added logout function in navbar

--- main.py
+++ main.py
@@ -151,6 +151,12 @@ def login():
 
     return render_template('login.html', form=form)
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
